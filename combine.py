68747336ab917006b0836d080334bcc052558106
AnakhaNandaPR <anakhanandapr@gmail.com>
2025-09-10 15:27:38 +0530

    normal

---
+++ combine.py
@@ -0,0 +1,11 @@
+FirstName=input("Enter your  first name:  ")
+SecondName=(input("Enter your second name:  "))
+combine=FirstName+SecondName
+print(combine)
+
+x=[1,2,5]
+y=[1,2,3]
+z=x
+print(x is not y)
+print(x is y)
+print(x is z)
